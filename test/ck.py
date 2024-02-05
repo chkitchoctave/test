ac1f45ab787b0abbbe4576167953e100c3a583b6
chkitchoctave <156809696+chkitchoctave@users.noreply.github.com>
2024-02-05 09:15:13 +0000

    Update ck.py

--- test/ck.py
+++ test/ck.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 import codecs
 from tabulate import tabulate  # Import the tabulate module
+import pandas_gbq
 
 api_url = "https://api.adswizz.com/domain/v7/reports/query"
 api_key = "3Ad8CexcwpQYcncwfUFu68ZKYrHuGvvJU7jjJwy2"
@@ -59,7 +60,7 @@ if r.status_code == 200:
 
         # Display the DataFrame as a table using tabulate
         table = tabulate(df, headers='keys', tablefmt='pretty')
-        print(r.text)
+        #print(r.text)
     except KeyError as e:
         print(f"KeyError: {e}")
         print("Response structure may have changed. Inspect the response:")
@@ -68,4 +69,29 @@ else:
     print(f"Error: {r.status_code}")
     #print(r.text)
 
+
+# Convert 'data' to a DataFrame
+df_data = pd.json_normalize(df['data'])
+
+def push_to_bigquery(df, project_id, dataset_id, table_id, if_exists='replace'):
+    destination_table = f"{project_id}.{dataset_id}.{table_id}"
+
+    try:
+        if not isinstance(df, pd.DataFrame):
+            raise ValueError("Input 'df' must be a pandas DataFrame.")
+
+        pandas_gbq.to_gbq(
+            df,
+            destination_table,
+            project_id=project_id,
+            if_exists=if_exists
+        )
+
+        print(f"Data pushed to BigQuery table: {destination_table}")
+    except Exception as e:
+        print(f"Error pushing data to BigQuery table {destination_table}: {str(e)}")
+
+
+
+push_to_bigquery(df, "octave-272909", "ck_sandbox", "test")
 
