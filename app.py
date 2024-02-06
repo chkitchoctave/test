970ae860840aacd04df9eb730d723f1ae76528c0
chkitchoctave <156809696+chkitchoctave@users.noreply.github.com>
2024-02-06 18:25:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
-import signal
-import sys
-from types import FrameType
-from flask import Flask
-from utils.logging import logger, flush  # Import flush function
-import subprocess
+import os
 
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello() -> str:
-    return "Hello, Chrissy World!"
-
-@app.route("/")
-def run_script() -> str:
-    # Run your Python script
-    try:
-        subprocess.run(["python", "test/ck.py"], check=True)
-        return "Script executed successfully"
-    except subprocess.CalledProcessError as e:
-        return f"Error executing script: {e}"
+@app.route('/')
+def hello_world():
+    target = os.environ.get('TARGET', 'World')
+    return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
